4c45b7eb2825cce2c1f6f455dca07778a0116b6e
venkateshmaryala <venkateshmaryala@gmail.com>
2021-10-31 13:19:57 -0700

    intiate print script

---
+++ 02-print.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+## To print some text on the screen we can sue echo command or printf command
+## we choose to go with echo command because of its less syntaxing
+
+#syntax
+#echo message to print
+
+echo Hello world
+echo welcome
