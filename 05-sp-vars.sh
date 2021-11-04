007383630ef12d0dd2389691a95b587a8daa4e11
venkateshmaryala <venkateshmaryala@gmail.com>
2021-11-04 00:48:00 -0700

    Intialize special vars update 4th spv1

--- 05-sp-vars.sh
+++ 05-sp-vars.sh
@@ -12,8 +12,8 @@ echo 3 = $3
 echo 4 = $4
 
 ## * and @ are giving all the arguments
-echo "* = $*"
-echo "@ = $@"
+echo "\e[31m* = $*"
+echo "@ = $@\e[0m"
 
 # #denotes number of arguments passed as inputs
 echo "# = $#"
