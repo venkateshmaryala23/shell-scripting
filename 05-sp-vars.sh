2d293ca72b38053e047d4ad19ba2e4847d83c7ac
venkateshmaryala <venkateshmaryala@gmail.com>
2021-11-04 00:46:02 -0700

    Intialize special vars update 4th spv

--- 05-sp-vars.sh
+++ 05-sp-vars.sh
@@ -6,8 +6,14 @@ echo 0 = $0
 echo 1 = $1
 #2 is for second argument
 echo 2 = $2
+#3 is for third argument
 echo 3 = $3
+#4 is for four arguments
+echo 4 = $4
 
+## * and @ are giving all the arguments
 echo "* = $*"
 echo "@ = $@"
+
+# #denotes number of arguments passed as inputs
 echo "# = $#"
