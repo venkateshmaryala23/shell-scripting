b4ed96339e1e0715c7ea36cc50fc316d0e69f601
venkateshmaryala <venkateshmaryala@gmail.com>
2021-11-04 23:36:46 -0700

    intializing abc function

---
+++ 07-functions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+## function should be always declared before using it, same like variable
+##so that is the reason, Function we always find in starting of the scripts
+
+function abc() {
+  echo this is abc
+  }
+
+#main program
+abc
+
+
