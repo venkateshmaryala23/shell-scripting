27532ee253a40ae7a2a76f969e4868e2c981a24d
venkateshmaryala <venkateshmaryala@gmail.com>
2021-11-06 10:06:20 -0700

    latest1

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 
 
 echo -n -e "\e[1mdownloading\e[0m......"
@@ -35,10 +35,10 @@ fi
 
 exit 5
 
-Update Liste IP address from 127.0.0.1 to 0.0.0.0 in config file
-Config file: /etc/mongod.conf
+#Update Liste IP address from 127.0.0.1 to 0.0.0.0 in config file
+#Config file: /etc/mongod.conf
 
-then restart the service
+#then restart the service
 
 # systemctl restart mongod
 Every Database needs the schema to be loaded for the application to work.
