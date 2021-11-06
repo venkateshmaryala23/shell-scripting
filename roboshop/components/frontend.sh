06a4da676dd8bc1840bb6f4c25d6fc6267df45a7
venkateshmaryala <venkateshmaryala@gmail.com>
2021-11-06 13:16:57 -0700

    updates

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -20,17 +20,19 @@ nstatus=$(systemctl is-active nginx.service)
 Service $nstatus
 
 Print "Downloading Html pages"
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>"$LOG"
 Stat $?
 
 Print "Remove Old Html Pages"
-rm -rf  /usr/share/nginx/html/*
+rm -rf  /usr/share/nginx/html/* &>>"$LOG"
 Stat $?
 
 Print "Extract Frontend Archive"
-unzip -d /usr/share/nginx/html /tmp/frontend.zip &>>"$LOG"
+unzip -d /tmp /tmp/frontend.zip &>>"$LOG"
 Stat $?
 
+#Print "copy files to Nginx path"
+#mv /tmp/front
 #mv frontend-main/* .
 #mv static/* .
 #rm -rf frontend-master static README.md
