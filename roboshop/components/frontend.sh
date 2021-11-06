3444cb6f46747711d3d15810599e8420b345fdc1
venkateshmaryala <venkateshmaryala@gmail.com>
2021-11-06 10:53:04 -0700

    Intialize common.sh file

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
 
-Print() {
-  echo -n -e "\e[1m$1\e[0m ....."
-  echo -e "\n\e[36m==============================$1==============================\e[0m" >>$LOG
-}
-
-Stat() {
-  if [ $1 -eq 0 ]; then
-   echo -e "\e[1;32mSUCCESS\e[0m"
-  else
-    echo -e "\e[1;31mFAILURE\e[0m"
-    echo -e "\e[1;33mScript is failed and plese check the details in $LOG file\e[0m"
-    exit 1
-  fi
-}
-
-LOG=/tmp/roboshop.log
-rm -f $LOG
-
 Print "Installing Nginx"
 yum install nginx -y &>>$LOG
 Stat $?
