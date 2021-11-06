c41eb3081877daa83c05b21ae51f57a9efcf5279
venkateshmaryala <venkateshmaryala@gmail.com>
2021-11-06 10:58:37 -0700

    updating common

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source components/common.sh
+
 Print "Downloading"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
 Stat $?
