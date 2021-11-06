a75823a7e260bb36bfb89251944ad44f38b467d1
venkateshmaryala <venkateshmaryala@gmail.com>
2021-11-06 13:43:54 -0700

    updates

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -28,7 +28,7 @@ rm -rf  /usr/share/nginx/html/* &>>"$LOG"
 Stat $?
 
 Print "Extract Frontend Archive"
-unzip -d -o /tmp /tmp/frontend.zip &>>"$LOG"
+unzip -o -d  /tmp /tmp/frontend.zip &>>"$LOG"
 Stat $?
 
 Print "copy files to Nginx path"
