975a34a1f9f5c2ac3f26cd80a2e5ed1b6b687839
venkateshmaryala <venkateshmaryala@gmail.com>
2021-11-07 04:59:01 -0800

    updating script

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -2,9 +2,14 @@
 
 COUNT=$(aws ec2 describe-instances --filters Name=tag:Name,Values=$1 |jq ".Reservations[].Instances[].PrivateIpAddress"| grep -v null | wc -l)
 
+ec2_state_code=$(aws ec2 describe-instances --filters Name=tag:Name,Values=cart |jq ".Reservations[].Instances[].State.Code")
+
+echo $ec2_state_code
+
 if [ $COUNT -eq 0 ]; then
   aws ec2 run-instances --image-id ami-0dc863062bc04e1de --instance-type t3.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
 else
   echo "Instance already exists"
+
 fi
 
