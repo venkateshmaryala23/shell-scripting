c25ab8c39383bb33767bcf0fa65fe8c43f4559ba
venkateshmaryala <venkateshmaryala@gmail.com>
2021-11-07 06:04:02 -0800

    update sg group

--- roboshop/custom-create-instance.sh
+++ roboshop/custom-create-instance.sh
@@ -8,7 +8,7 @@ ec2_state_code=$(aws ec2 describe-instances --filters Name=tag:Name,Values=$1 |j
 
 if [ $COUNT -eq 0 ]; then
      echo "There is no instance existed.... so creating instance for your $1 ec2 instance"
-     aws ec2 run-instances --image-id ami-0dc863062bc04e1de --instance-type t3.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
+     aws ec2 run-instances --image-id ami-0dc863062bc04e1de --instance-type t3.micro --security-group-ids sg-07624ce53dbdfb0f8 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
 else
   #echo "Instance already exists"
   for x in $ec2_state_code;do
