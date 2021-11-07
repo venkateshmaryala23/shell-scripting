d7fec232561b2b802fc76d5660f25e6acb9b328c
venkateshmaryala <venkateshmaryala@gmail.com>
2021-11-07 04:00:45 -0800

    updating if function

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -2,6 +2,10 @@
 
 COUNT=$(aws ec2 describe-instances --filters Name=tag:Name,Values=$1 |jq ".Reservations[].Instances[].InstanceId" | wc -l)
 
-echo $COUNT
 
-#aws ec2 run-instances --image-id ami-0dc863062bc04e1de --instance-type t3.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
+if [ $COUNT -eq 0 ]; then
+  aws ec2 run-instances --image-id ami-0dc863062bc04e1de --instance-type t3.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
+else
+  echo "Instance already exists"
+fi
+
