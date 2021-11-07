744a7b9a5ec8c2c593c9f5d33ed3c95471a4e145
venkateshmaryala <venkateshmaryala@gmail.com>
2021-11-07 02:23:57 -0800

    updating with tag details in jq123

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aws ec2 run-instances --image-id ami-0dc863062bc04e1de --instance-type t3.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]"
+aws ec2 run-instances --image-id ami-0dc863062bc04e1de --instance-type t3.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
