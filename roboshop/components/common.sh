b694a6f2fcf79832341a3ec6044517f70d366e78
venkateshmaryala <venkateshmaryala@gmail.com>
2021-11-11 00:43:11 -0800

    space fix issues

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 Print() {
-  LSPACE=$(echo $1 | awk '{print length}')
-  SPACE=$(($MSPACE-$LSPACE))
+  LSPACE=$(echo "$1" | awk "{print length}")
+  SPACE=$("${MSPACE}"-"${LSPACE}")
   SPACES=""
-  while [ $SPACE -gt 0 ]; do
+  while [ "$SPACE" -gt 0 ]; do
      SPACES="$SPACES$(echo ' ')"
-     SPACE=$(($SPACE-1))
+     SPACE=$(${(SPACE-1)})
   done
   echo -n -e "\e[1m$1${SPACES}\e[0m ....."
-  echo -e "\n\e[36m==============================$1==============================\e[0m" >>$LOG
+  echo -e "\n\e[36m==============================$1==============================\e[0m" >>"$LOG"
 }
 
 Stat() {
-  if [ $1 -eq 0 ]; then
+  if [ "$1" -eq 0 ]; then
    echo -e "\e[1;32mSUCCESS\e[0m"
   else
     echo -e "\e[1;31mFAILURE\e[0m"
