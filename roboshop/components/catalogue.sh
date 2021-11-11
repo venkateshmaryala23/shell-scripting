e93bf3c5c1c6e1a1f13e47904ec76247ff0db74e
venkateshmaryala <venkateshmaryala@gmail.com>
2021-11-10 21:26:19 -0800

    Mline

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source components/common.sh
 
-MLINE=cat $0
+echo cat $0
 #MLINE=$(cat $0 | grep ^Print | awk -F  '"' '{print $2}' | awk '{ print length }' | sort | tail -1)
 
 echo $MLINE
