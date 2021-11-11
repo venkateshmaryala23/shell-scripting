9f2dc8d7d9322ca1c71669e2743695dafbfa71f4
venkateshmaryala <venkateshmaryala@gmail.com>
2021-11-10 22:20:42 -0800

    space fix issues

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -3,7 +3,6 @@
 Print() {
   LSPACE=$(echo $1 | awk '{print length}')
   SPACES=$(($MSPACE-$LSPACE))
-  echo $SPACES
   echo -n -e "\e[1m$1${SPACES}\e[0m ....."
   echo -e "\n\e[36m==============================$1==============================\e[0m" >>$LOG
 }
