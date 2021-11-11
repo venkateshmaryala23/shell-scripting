c4d82897285642fe61a842ee49b66554d144ef2f
venkateshmaryala <venkateshmaryala@gmail.com>
2021-11-10 21:57:35 -0800

    update sorting

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source components/common.sh
 
-cat $0 | grep ^Print | awk -F '"' '{print $2}' | awk '{print length}' | sort | tail -2
+cat $0 | grep ^Print | awk -F '"' '{print $2}' | awk '{print length}' | sort | tail -10
 
 exit
 #echo "cat $0 | grep ^Print"
