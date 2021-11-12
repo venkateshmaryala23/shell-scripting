5851177536a31e4c941a35b60c2b26a50dcf337a
venkateshmaryala <venkateshmaryala@gmail.com>
2021-11-12 05:30:47 -0800

    updating latest image ami-0855cab4944392d0a

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -3,7 +3,7 @@
 COUNT=$(aws ec2 describe-instances --filters Name=tag:Name,Values=$1 |jq ".Reservations[].Instances[].PrivateIpAddress"| grep -v null | wc -l)
 
 if [ $COUNT -eq 0 ]; then
-       aws ec2 run-instances --image-id ami-077fb3e62ddf0fa9a --instance-type t3.micro --security-group-ids sg-07624ce53dbdfb0f8 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
+       aws ec2 run-instances --image-id ami-0855cab4944392d0a --instance-type t3.micro --security-group-ids sg-07624ce53dbdfb0f8 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
 else
   echo "Instance already exists"
 fi
