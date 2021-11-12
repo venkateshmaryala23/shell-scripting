1c87bf0c017d8ab1acfb633d8141e8bb5f4d7929
venkateshmaryala <venkateshmaryala@gmail.com>
2021-11-12 05:56:14 -0800

    done script for redis

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -3,14 +3,24 @@ source components/common.sh
 
 MSPACE=$(cat $0 | grep ^Print | awk -F '"' '{print $2}' | awk '{print length}' | sort | tail -1)
 
-
+Print "Install Redis Repos "
 # yum install epel-release yum-utils -y
-# yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y
-# yum-config-manager --enable remi
-# yum install redis -y
-Update the BindIP from 127.0.0.1 to 0.0.0.0 in config file /etc/redis.conf & /etc/redis/redis.conf
+yum install yum-utils http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y &>>"$LOG"
+Stat $?
 
-Start Redis Database
+Print "Enable Redis"
+yum-config-manager --enable remi &>>"$LOG"
+Stat $?
 
-# systemctl enable redis
-# systemctl start redis
+Print "Install Redis"
+yum install redis -y &>>"$LOG"
+Stat $?
+
+#Update the BindIP from 127.0.0.1 to 0.0.0.0 in config file /etc/redis.conf & /etc/redis/redis.conf
+Print "Updating Redis Listen Address"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf &>>"$LOG"
+Stat $?
+
+Print "Starting and enabling Redis"
+systemctl enable redis && systemctl start redis &>>"$LOG"
+Stat $?
