d68fe0ee95e9e85b6dca1a9ad3e804fb4ed05fff
venkateshmaryala <venkateshmaryala@gmail.com>
2021-11-12 07:35:13 -0800

    update User

--- roboshop/components/user.sh
+++ roboshop/components/user.sh
@@ -6,25 +6,7 @@ MSPACE=$(cat $0 | grep ^Print | awk -F '"' '{print $2}' | awk '{print length}' |
 COMPONENT_NAME=User
 COMPONENT=user
 
-#Now, lets set up the service with systemctl.
-
-# mv /home/roboshop/user/systemd.service /etc/systemd/system/user.service
-# systemctl daemon-reload
-# systemctl start user
-# systemctl enable user
-
-
-Print "Update DNS records in SystemD Config"
-sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/catalogue/systemd.service &>>"$LOG"
-Stat $?
-
-Print "Copy SystemD file"
-mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
-Stat $?
-
-Print "System Catalogue Service"
-systemctl daemon-reload &>>"$LOG" && systemctl start catalogue &>>"$LOG" && systemctl enable catalogue &>>"$LOG"
-Stat $?
+NODEJS
 
 sleep 5
 
