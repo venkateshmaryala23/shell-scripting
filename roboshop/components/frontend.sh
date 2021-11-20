728b974e0689717c3bfefa10f21b51544563a3a1
venkateshmaryala <venkateshmaryala@gmail.com>
2021-11-19 22:04:31 -0800

    update

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -31,6 +31,10 @@ Print "Enabling Nginx"
 systemctl enable nginx &>>"$LOG"
 Stat $?
 
+Print "Update Nginx Roboshop Config file"
+sed -i -e '/catalogue/ s/localhost/catalogue.roboshop.internal/' -e  '/user/ s/localhost/user.roboshop.internal/' -e '/cart/ s/localhost/cart.roboshop.internal/' -e '/shipping/ s/localhost/shipping.roboshop.internal/' -e '/payment/ s/localhost/payment.roboshop.internal/' /etc/nginx/default.d/roboshop.conf &>>"$LOG"
+
+
 Print "Starting Nginx"
 systemctl start nginx &>>"$LOG"
 systemctl --type=service | grep nginx &>>"$LOG"
