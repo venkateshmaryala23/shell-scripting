ba6ce635b13336707ec9ea170a1977ea5728938c
venkateshmaryala <venkateshmaryala@gmail.com>
2021-11-20 10:11:27 -0800

    update

--- roboshop/delete-records.sh
+++ roboshop/delete-records.sh
@@ -3,7 +3,6 @@ DELETE_RECORD (){
   record_list=$(aws route53 list-resource-record-sets --hosted-zone-id Z05238653F1UHIRHF2JKO --query "ResourceRecordSets[?Type == 'A'].Name" | xargs)
   echo $record_list
 
-
 }
 
 DELETE_RECORD
