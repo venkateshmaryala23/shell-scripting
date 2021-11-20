79c38f41da0e7b27857f56879f3147dbc598b133
venkateshmaryala <venkateshmaryala@gmail.com>
2021-11-20 02:32:23 -0800

    update

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -4,16 +4,15 @@ CREATE() {
      if [ $COUNT -eq 0 ]; then
            #echo "Creating Instance - $1"
            aws ec2 run-instances --image-id ami-0855cab4944392d0a --instance-type t3.micro --security-group-ids sg-07624ce53dbdfb0f8 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq &>/dev/null
-     #else
-          # echo "$1 Instance already exist"
+     else
+          echo -e "\[1:33m$1 Instance already exist\[0m"
      fi
-     #echo "==========================="
+
      sleep 5
      IP=$(aws ec2 describe-instances --filters Name=tag:Name,Values=$1 |jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null | xargs)
      sed -e "s/DNSNAME/$1.roboshop.internal/" -e "s/IPADRESS/${IP}/" record.json >/tmp/record.json
-     #echo "updating dns record for $1 instance"
      aws route53 change-resource-record-sets --hosted-zone-id Z05238653F1UHIRHF2JKO --change-batch file:///tmp/record.json | jq &>/dev/null
-     #echo "==========================="
+
 }
 
 if [ "$1" == "all" ]; then
@@ -22,6 +21,8 @@ if [ "$1" == "all" ]; then
     echo "Creating Instance - $component"
     CREATE $component
   done
+else
+  CREATE $i
 fi
 
 
