906677a2c8ffd14edf2884fbcf319220cb16de3d
venkateshmaryala <venkateshmaryala@gmail.com>
2021-11-20 01:54:04 -0800

    update

--- roboshop/custom-create-instance.sh
+++ roboshop/custom-create-instance.sh
@@ -1,29 +1,25 @@
 #!/bin/bash
 CREATE () {
   COUNT=$(aws ec2 describe-instances --filters Name=tag:Name,Values=$1 |jq ".Reservations[].Instances[].PrivateIpAddress"| grep -v null | wc -l)
-
   ec2_state_code=$(aws ec2 describe-instances --filters Name=tag:Name,Values=$1 |jq ".Reservations[].Instances[].State.Code")
-
-  #echo $ec2_state_code
 
   if [ $COUNT -eq 0 ]; then
      echo "There is no instance existed.... so creating instance for your $1 ec2 instance"
      aws ec2 run-instances --image-id ami-0dc863062bc04e1de --instance-type t3.micro --security-group-ids sg-07624ce53dbdfb0f8 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
   else
-     #echo "Instance already exists"
      for x in $ec2_state_code;do
         #echo $x
         if [ $x == 0 ]; then
           #"\e[1;32mSUCCESS\e[0m"
-          echo "\e[1;32m$1 Instance is exists but it is in\e[0m" pending status
+          echo "\e[1;32m$1 Instance is exists but it is in pending status\e[0m"
         elif [ $x == 16 ]; then
-          echo  -e "\e[1;32m$1 Instance is already exists, It is in\e[0m" running state
+          echo  -e "\e[1;32m$1 Instance is already exists, It is in running state \e[0m"
         elif [ $x ==  32 ]; then
-          echo "$1 Instance is already exist but its statu is shutting-down"
+          echo "\e[1;32m$1 Instance is already exist but its statu is shutting-down \e[0m"
         elif [ $x == 64 ]; then
-          echo "$1 Instance is already exist but its status is stopping"
+          echo "\e[1;32m$1 Instance is already exist but its status is stopping \e[0m"
         elif [ $x == 80 ]; then
-          echo "$1 Instance is already exist but the instance is stopped"
+          echo "\e[1;32m$1 Instance is already exist but the instance is stopped \e[0m""
         fi
     done
   fi
