b06b2b699ab90dca294d1bb1b97878aeab22a544
venkateshmaryala <venkateshmaryala@gmail.com>
2021-11-20 13:12:15 -0800

    update

--- roboshop/delete-instance.sh
+++ roboshop/delete-instance.sh
@@ -4,11 +4,14 @@ DELETE() {
   IP=$(aws ec2 describe-instances --filters Name=tag:Name,Values=$1 |jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null | xargs)
   ID=$(aws ec2 describe-instances --filters Name=tag:Name,Values=cart |jq ".Reservations[].Instances[].InstanceId" | xargs)
   sed -e "s/DNSNAME/$1.roboshop.internal/" -e "s/IPADRESS/${IP}/" delete_record.json >/tmp/drecord.json
+  recordset=$(aws route53 list-resource-record-sets --hosted-zone-id Z05238653F1UHIRHF2JKO --query "ResourceRecordSets[?Type == 'A'].Name" | xargs)
+  echo $recordset
   echo $IP
   echo $ID
+  exit
   if [ -z "$IP" ]; then
     echo "There is no $1 dns record to delete"
-  else
+  elif []
     aws route53 change-resource-record-sets --hosted-zone-id Z05238653F1UHIRHF2JKO --change-batch file:///tmp/drecord.json | jq &>/dev/null
     if [ $? == 0 ]; then
       echo removed dns record for $1
