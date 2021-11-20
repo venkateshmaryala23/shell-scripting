8276f0abd6ffbb08aa15b570a69ef1bae88a864f
venkateshmaryala <venkateshmaryala@gmail.com>
2021-11-20 00:57:42 -0800

    update

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -6,10 +6,11 @@ for i in cart catalogue frontend mongodb mysql payment rabbitmq redis shipping u
   if [ $COUNT -eq 0 ]; then
        aws ec2 run-instances --image-id ami-0855cab4944392d0a --instance-type t3.micro --security-group-ids sg-07624ce53dbdfb0f8 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
   else
-  echo "Instance already exists"
+  echo " $i Instance already exists"
  fi
  sleep 5
  IP=$(aws ec2 describe-instances --filters Name=tag:Name,Values=$i |jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null | xargs)
  sed -e "s/DNSNAME/$i.roboshop.internal/" -e "s/IPADRESS/${IP}/" record.json >/tmp/record.json
+ echo "updating dns record for $i instance"
  aws route53 change-resource-record-sets --hosted-zone-id Z05238653F1UHIRHF2JKO --change-batch file:///tmp/record.json
 done
