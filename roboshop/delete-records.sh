55b62900781a0c0f8e3a93d94a1862d1d49b8424
venkateshmaryala <venkateshmaryala@gmail.com>
2021-11-20 10:44:10 -0800

    update

--- roboshop/delete-records.sh
+++ roboshop/delete-records.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 DELETE_RECORD (){
-  record_list=$(aws route53 list-resource-record-sets --hosted-zone-id Z05238653F1UHIRHF2JKO --query "ResourceRecordSets[?Type == 'A'].Name" | xargs)
-  echo record_list
+  aws route53 list-resource-record-sets --hosted-zone-id Z05238653F1UHIRHF2JKO --query "ResourceRecordSets[?Type == 'A'].Name" &>/tmp/recordlist.txt
+  sed -i -e 's/.",//' -e 's/"//' -e 's/."//' /tmp/recordlist.txt
+  for recordset in 'cat /tmp/recordlist.txt';do
+    echo $i
 
 }
 
