39f98d6cbf3e8e2627dd1d3321de41ebde54055f
venkateshmaryala23 <venkateshmaryala23@gmail.com>
2021-12-04 00:06:29 +0530

    update

--- roboshop/delete-instance.sh
+++ roboshop/delete-instance.sh
@@ -1,24 +1,20 @@
 #!/bin/bash
 DELETE() {
+
   COUNT=$(aws ec2 describe-instances --filters Name=tag:Name,Values=$1 | jq ".Reservations[].Instances[].PrivateIpAddress"| grep -v null | wc -l)
+
   IP=$(aws ec2 describe-instances --filters Name=tag:Name,Values=$1 |jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null | xargs)
-  ID=$(aws ec2 describe-instances --filters Name=tag:Name,Values=$1 |jq ".Reservations[].Instances[].InstanceId" | xargs)
+
   sed -e "s/DNSNAME/$1.roboshop.internal./" -e "s/IPADRESS/${IP}/" delete_record.json >/tmp/drecord.json
+
   recordset=$(aws route53 list-resource-record-sets --hosted-zone-id Z05238653F1UHIRHF2JKO --query "ResourceRecordSets[?Name == '$1.roboshop.internal.']" | jq ".[].Name" | xargs)
   current_dnsname=$(cat /tmp/drecord.json | jq ".Changes[].ResourceRecordSet.Name" |xargs)
-  #echo $recordset
-  #echo $current_dnsname
-  # echo $IP
-  #echo $ID
+
   DNS_IP=$(aws route53 list-resource-record-sets --hosted-zone-id Z05238653F1UHIRHF2JKO --query "ResourceRecordSets[?Name == '$1.roboshop.internal.']" | jq ".[].ResourceRecords[].Value"|grep -v null |xargs)
-# echo $DNS_IP
+
 
   if [ -z "$IP" ] && [ -z "$DNS_IP" ]; then
      echo -e "\e[1;33mThere is no dns record existed for $1 to delete\e[0m"
- # elif [ -z "$DNS_IP" ]; then
- #     sed -e "s/DNSNAME/$1.roboshop.internal./" -e "s/IPADRESS/${DNS_IP}/" delete_record.json >/tmp/drecord.json
-  #    aws route53 change-resource-record-sets --hosted-zone-id Z05238653F1UHIRHF2JKO --change-batch file:///tmp/drecord.json | jq &>/dev/null
-   #   echo  "\e[1;33m no instance there but dns record existed so deleting dns for $1\e[0m"
   elif [ "$recordset" == "$current_dnsname" ] || [ -z "$DNS_IP" ] ;then
     sed -e "s/DNSNAME/$1.roboshop.internal./" -e "s/IPADRESS/${DNS_IP}/" delete_record.json >/tmp/drecord.json
     aws route53 change-resource-record-sets --hosted-zone-id Z05238653F1UHIRHF2JKO --change-batch file:///tmp/drecord.json | jq &>/dev/null
@@ -31,7 +27,7 @@ DELETE() {
 
   sleep 5
   if [ $COUNT -ne 0 ]; then
-
+    ID=$(aws ec2 describe-instances --filters Name=tag:Name,Values=$1 |jq ".Reservations[].Instances[].InstanceId" | xargs)
     aws ec2 terminate-instances --instance-ids $ID
     echo -e "\e[1;33mDeleted Instance - $ID ---$IP---$current_dnsname"
   else
@@ -40,6 +36,7 @@ DELETE() {
   fi
  }
 
+#main
 if [ "$1" == "all" ]; then
   ALL=(frontend mongodb catalogue redis user cart mysql shipping rabbitmq payment)
   for component in ${ALL[*]}; do
